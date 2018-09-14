725b7dacdd26545c6b5079a3a6f8c79de219b2a7
WeakMill98 <jerrybao1998@outlook.com>
2018-09-14 14:11:20 -0600

    Added in data from activity

--- JojoCharacters/app/src/main/java/com/example/jerry/jojocharacters/CharacterDetailsActivity.java
+++ JojoCharacters/app/src/main/java/com/example/jerry/jojocharacters/CharacterDetailsActivity.java
@@ -1,15 +1,26 @@
 package com.example.jerry.jojocharacters;
 
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CharacterDetailsActivity extends AppCompatActivity {
+
+    TextView nametextView;
+    TextView standTextView;
+    TextView duwangTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_details);
 
+        nametextView = (TextView) findViewById(R.id.nameTextView);
+        standTextView = (TextView) findViewById(R.id.standTextView);
+        duwangTextView = (TextView) findViewById(R.id.duwangTextView);
+
         String name = (String) getIntent().getExtras().get("characterName");
+        nametextView.setText(name);
     }
 }
