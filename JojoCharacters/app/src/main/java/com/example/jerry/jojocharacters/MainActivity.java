085dc7b0f09c1e6bcd6197dc76f78ef2bbb26c65
Jerry_Bao <jerrybao1998@outlook.com>
2018-09-13 22:40:51 -0600

    Added in list view with text

--- JojoCharacters/app/src/main/java/com/example/jerry/jojocharacters/MainActivity.java
+++ JojoCharacters/app/src/main/java/com/example/jerry/jojocharacters/MainActivity.java
@@ -7,7 +7,12 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String characters[] = new String[] {"D'Arby", "Steely Dan", "Kakyoin", "Polnareff", "Koichi"};
+    String characters[] = new String[] {
+            "D'Arby", "Steely Dan", "Kakyoin", "Polnareff", "Koichi",
+            "D'Arby", "Steely Dan", "Kakyoin", "Polnareff", "Koichi",
+            "D'Arby", "Steely Dan", "Kakyoin", "Polnareff", "Koichi",
+            "D'Arby", "Steely Dan", "Kakyoin", "Polnareff", "Koichi"
+    };
 
     ListView listView;
 
@@ -19,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
         // Simple list item 1 is for text only, item 2 is text and picture
         ArrayAdapter<String> characterAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, characters);
+        listView.setAdapter(characterAdapter);
     }
 }
